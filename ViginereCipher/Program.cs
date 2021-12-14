0685c531dbf7f321fcc3d1d35eb18a9e84e80fb2
nicholas.finefter <nicholas.finefter@GMRDL1>
2021-12-14 14:48:40 -0800

    Add project files.

---
+++ ViginereCipher/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ViginereCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a string!");
+            string input = Console.ReadLine();
+        }
+    }
+}
