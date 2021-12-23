430c18fb080df2c13bf3b2454597385048a40a72
nicholas.finefter <nfinefter@gmail.com>
2021-12-22 17:18:53 -0800

    Finished Decrypt, everything

--- ViginereCipher/Program.cs
+++ ViginereCipher/Program.cs
@@ -4,6 +4,25 @@ namespace ViginereCipher
 {
     class Program
     {
+        static char[,] tabulaRecta;
+
+        static void PrintTabulaRecta(char[,] tabulaRecta)
+        {
+            string print = "";
+
+            for (int i = 0; i < tabulaRecta.GetLength(0); i++)
+            {
+                for (int j = 0; j < tabulaRecta.GetLength(1); j++)
+                {
+                    print += tabulaRecta[i, j];
+
+                }
+                Console.WriteLine(print);
+                print = "";
+
+            }
+        }
+
         static string Encrypt(string input, string key)
         {
             string encryptedMsg = "";
@@ -15,7 +34,7 @@ namespace ViginereCipher
 
             string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-            char[,] tabulaRecta = new char[columns, rows];
+            tabulaRecta = new char[columns, rows];
 
             int count = 0;
 
@@ -36,32 +55,39 @@ namespace ViginereCipher
                         finalLetter = (char)(96);
                         count = 0;
                     }
-                    finalLetter++;
                 }
                 offset++;
                 count = offset;
             }
+            Console.WriteLine(" ");
 
-            int indexCount = 0;
+            PrintTabulaRecta(tabulaRecta);
+
+            Console.WriteLine(" ");
+
             int keyCount = 0;
 
 
 
-                for (int j = 0; j < input.Length; j++)
+            for (int j = 0; j < input.Length; j++)
+            {
+                char c = newString[j];
+
+                if (!char.IsLetter(c))
                 {
-                if (newString[indexCount] == ' ')
-                {
-                    //newString[indexCount]
+                    encryptedMsg += c;
                 }
-                    int index = (char)(newString[indexCount] - 97);
+                else
+                {
+                    int index = (char)(newString[j] - 97);
                     int keyIndex = (char)(key[keyCount] - 97);
+                    keyCount++;
 
                     char encryptedLetter = tabulaRecta[index, keyIndex];
 
                     encryptedMsg += encryptedLetter;
-                    indexCount++;
-                
-                keyCount++;
+                }
+
                 if (keyCount >= key.Length)
                 {
                     keyCount = 0;
@@ -71,14 +97,73 @@ namespace ViginereCipher
             return encryptedMsg;
         }
 
+        static string Decrypt(string encryptedMsg, string key, char[,] tabulaRecta)
+        {
+            string decryptedMsg = "";
+
+
+
+            //Loop for encryptedMsg.Length
+
+            int keyCount = 0;
+
+            for (int i = 0; i < encryptedMsg.Length; i++)
+            {
+                char c = encryptedMsg[i];
+
+                if (!char.IsLetter(c))
+                {
+                    decryptedMsg += c;
+                }
+                else
+                {
+                    int keyIndex = (char)(key[keyCount] - 97);
+                    int column = FindColumnIndex(keyIndex, encryptedMsg[i], tabulaRecta);
+                    decryptedMsg += (char)(column + 'a');
+                    keyCount++;
+
+                    if (keyCount >= key.Length)
+                    {
+                        keyCount = 0;
+                    }
+                }
+              
+                
+            } 
+
+            return decryptedMsg;
+        }
+
+        //We have the row, we have the table, we have the char we are looking for
+        //Loop through the table (just the row) and continue looping until table[row, i] == character
+        static int FindColumnIndex(int rowIndex, char letter, char[,] tabulaRecta)
+        {
+            for (int j = 0; j < tabulaRecta.GetLength(1); j++)
+            {
+                if (letter == tabulaRecta[rowIndex, j])
+                {
+                    return j;
+                }
+
+            }
+
+            return -1;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Give a string!");
+            Console.WriteLine("Give String");
             string input = Console.ReadLine();
-            input = "Common sense is not so common.";
+            //input = "common sense is not so common";
+
             string key = "pizza";
 
-            Console.WriteLine(Encrypt(input, key));
+            string msg = Encrypt(input, key);
+
+            Console.WriteLine(msg);
+
+            Console.WriteLine(Decrypt(msg, key, tabulaRecta));
 
         }
     }
